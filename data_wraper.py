921069279b67bc9934e750c741bf91b3d4ce3568
Maxime PETITPOISSON <m-petitpoisson@outlook.fr>
2023-05-31 11:36:05 +0200

    fix cropping and more

--- data_wraper.py
+++ data_wraper.py
@@ -1,133 +1,164 @@
 from scipy.io import loadmat
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-class Map:
-    def __init__(self, image, coeff, mask):
-        self.image = image
-        self.coeff = coeff
-        self.mask = mask
-    def get_center(self):
-        # return the center of the mask
-        mask = self.mask
+class Dataset:
+    def __init__(self, file_paths):
+        self.nb_data = 0
+        self.files = []
+        for file_path in file_paths:
+            file = loadmat(file_path)
+            self.files.append(file)
+            self.nb_data += len(file['Map1_outForCeline'][0])
+
+        self.map1_list = np.zeros((self.nb_data, 512, 512))
+        self.coeff1_list = np.zeros((self.nb_data))
+        self.mask1_list = np.zeros((self.nb_data, 512, 512))
+
+        self.map2_list = np.zeros((self.nb_data, 512, 512))
+        self.coeff2_list = np.zeros((self.nb_data))
+        self.mask2_list = np.zeros((self.nb_data, 512, 512))
+
+        self.map3_list = np.zeros((self.nb_data, 512, 512))
+        self.coeff3_list = np.zeros((self.nb_data))
+        self.mask3_list = np.zeros((self.nb_data, 512, 512))
+
+        self.combined_list = np.zeros((self.nb_data, 512, 512))
+        self.azimut_list = np.zeros((self.nb_data))
+
+        self.add_data()
+
+        self.files = []
+                
+        self.apply_coeff()
+    
+    def add_data(self):
+        i = 0
+        for file in self.files:
+            map1 = file['Map1_outForCeline'][0]
+            coeff1 = file['coef1_outForCeline'][0]
+            mask1 = file['mask1_outForceline'][0]
+            map2 = file['Map2_outForCeline'][0]
+            coeff2 = file['coef2_outForCeline'][0]
+            mask2 = file['mask2_outForceline'][0]
+            map3 = file['Map3_outForCeline'][0]
+            coeff3 = file['coef3_outForCeline'][0]
+            mask3 = file['mask3_outForceline'][0]
+            combined = file['Acombine_outForCeline'][0]
+            azimut = file['OUT_Fa_celine'][0]
+            for j in range(len(file['Map1_outForCeline'][0])):
+                self.map1_list[i] = map1[j]
+                self.coeff1_list[i] = coeff1[j]
+                self.mask1_list[i] = mask1[j]
+                self.map2_list[i] = map2[j]
+                self.coeff2_list[i] = coeff2[j]
+                self.mask2_list[i] = mask2[j]
+                self.map3_list[i] = map3[j]
+                self.coeff3_list[i] = coeff3[j]
+                self.mask3_list[i] = mask3[j]
+                self.combined_list[i] = combined[j]
+                self.azimut_list[i] = azimut[j]
+                i += 1
+    
+    def apply_coeff(self):
+        self.map1_list = self.map1_list * self.coeff1_list[:, None, None]
+        self.map2_list = self.map2_list * self.coeff2_list[:, None, None]
+        self.map3_list = self.map3_list * self.coeff3_list[:, None, None]
+
+    def preprocess_data(self):
+        def pretreatment(x):
+            x = np.abs(x)
+            x_nonzero = x[x != 0]
+            min_val = np.min(x_nonzero)
+            x[x == 0] = min_val
+            x = np.log10(x)
+            return x
+
+        self.map1_list = pretreatment(self.map1_list)
+        self.map2_list = pretreatment(self.map2_list)
+        self.map3_list = pretreatment(self.map3_list)
+        self.combined_list = pretreatment(self.combined_list)
+
+        min_val = np.min([np.min(self.map1_list), np.min(self.map2_list), np.min(self.map3_list), np.min(self.combined_list)])
+        max_val = np.max([np.max(self.map1_list), np.max(self.map2_list), np.max(self.map3_list), np.max(self.combined_list)])
+        self.map1_list = (self.map1_list - min_val) / (max_val - min_val)
+        self.map2_list = (self.map2_list - min_val) / (max_val - min_val)
+        self.map3_list = (self.map3_list - min_val) / (max_val - min_val)
+        self.combined_list = (self.combined_list - min_val) / (max_val - min_val)
+
+    
+    def crop_data(self, crop_size):
+        map1_list_cropped = np.zeros((len(self.map1_list), crop_size, crop_size))
+        map2_list_cropped = np.zeros((len(self.map2_list), crop_size, crop_size))
+        map3_list_cropped = np.zeros((len(self.map3_list), crop_size, crop_size))
+        combined_list_cropped = np.zeros((len(self.combined_list), crop_size, crop_size))
+        for i in range(len(self.map1_list)):
+            center = self.get_center(i)
+            box = np.array([center[0] - crop_size / 2, center[1] - crop_size / 2, center[0] + crop_size / 2, center[1] + crop_size / 2])
+            box = box.astype(int)
+            box = np.clip(box, 0, 512)
+            map1_list_cropped[i] = self.crop_map(self.map1_list[i], box, crop_size)
+            map2_list_cropped[i] = self.crop_map(self.map2_list[i], box, crop_size)
+            map3_list_cropped[i] = self.crop_map(self.map3_list[i], box, crop_size)
+            combined_list_cropped[i] = self.crop_map(self.combined_list[i], box, crop_size)
+            
+        self.map1_list = map1_list_cropped
+        self.map2_list = map2_list_cropped
+        self.map3_list = map3_list_cropped
+        self.combined_list = combined_list_cropped
+
+    def get_center(self, i):
+        mask = self.mask3_list[i]
         center = np.array([0, 0])
         center[0] = np.sum(mask, axis=1).argmax()
         center[1] = np.sum(mask, axis=0).argmax()
         return center
-    def crop_map(self, center, crop_size=None):
-        if crop_size == None:
-            return
-        image = self.image
-        box = np.array([center[0] - crop_size / 2, center[1] - crop_size / 2, center[0] + crop_size / 2, center[1] + crop_size / 2])
-        box = box.astype(int)
+
+    def crop_map(self, image, box, crop_size):
         image = image[box[0]:box[2], box[1]:box[3]]
-        self.image = image
-        # padding
-        pad = np.ones((crop_size, crop_size))
+        pad = np.ones((crop_size, crop_size)) * np.mean(image)
         pad[0:image.shape[0], 0:image.shape[1]] = image
-        self.image = np.array(pad)
-        return
-
-
-class Data:
-    def __init__(self, map1, map2, map3, combined, azimut):
-        self.map1 = map1
-        self.map2 = map2
-        self.map3 = map3
-        self.combined = combined
-        self.azimut = azimut
-    
-    def crop_maps(self, crop_size=None):
-        if crop_size == None:
-            return
-        center = self.map3.get_center()
-        self.map1.crop_map(center, crop_size)
-        self.map2.crop_map(center, crop_size)
-        self.map3.crop_map(center, crop_size)
-        self.combined.crop_map(center, crop_size)
-        return
-
-class Dataset:
-    def __init__(self, file_path = None, crop=False):
-        self.data_list = []
-        self.data_num = 0
-        self.crop = crop
-
-        if file_path != None:
-            self.add_data(file_path)
-    
-    def add_data(self, file_path):
-        file = loadmat(file_path)
-        map_list_1 = file['Map1_outForCeline'][0]
-        coeff_list_1 = file['coef1_outForCeline'][0]
-        mask_list_1 = file['mask1_outForceline'][0]
-        map_list_2 = file['Map2_outForCeline'][0]
-        coeff_list_2 = file['coef2_outForCeline'][0]
-        mask_list_2 = file['mask2_outForceline'][0]
-        map_list_3 = file['Map3_outForCeline'][0]
-        coeff_list_3 = file['coef3_outForCeline'][0]
-        mask_list_3 = file['mask3_outForceline'][0]
-        combined_list = file['Acombine_outForCeline'][0]
-        azimut_list = file['OUT_Fa_celine'][0]
-        for i in range(len(map_list_1)):
-            map1 = Map(map_list_1[i], coeff_list_1[i], mask_list_1[i])
-            map2 = Map(map_list_2[i], coeff_list_2[i], mask_list_2[i])
-            map3 = Map(map_list_3[i], coeff_list_3[i], mask_list_3[i])
-            combined = Map(combined_list[i], None, [])
-            azimut = azimut_list[i]
-            data = Data(map1, map2, map3, combined, azimut)
-
-            if self.crop == True:
-                data.crop_maps(100)
-
-            self.data_list.append(data)
-        self.data_num += len(map_list_1)
-    
+        return pad
+        
     def export_dataframe(self):
-        data_list = []
-        for data in self.data_list:
-            data_list.append([data.map1.image, data.map1.coeff, data.map1.mask, data.map2.image, data.map2.coeff, data.map2.mask, data.map3.image, data.map3.coeff, data.map3.mask, data.combined.image, data.azimut])
-        df = pd.DataFrame(data_list, columns=['Map1', 'Coeff1', 'Mask1', 'Map2', 'Coeff2', 'Mask2', 'Map3', 'Coeff3', 'Mask3', 'Combined', 'Azimut'])
+        data = {'map1': self.map1_list.tolist(), 'map2': self.map2_list.tolist(), 'map3': self.map3_list.tolist(), 'combined': self.combined_list.tolist()}
+        df = pd.DataFrame(data)
+        #df.to_csv('data.csv', index=False)
         return df
     
     def plot_map(self, data_index, map_index):
-        import matplotlib.pyplot as plt
-        data = self.data_list[data_index]
         if map_index == 1:
-            map = data.map1
+            map = self.map1_list[data_index]
         elif map_index == 2:
-            map = data.map2
+            map = self.map2_list[data_index]
         elif map_index == 3:
-            map = data.map3
+            map = self.map3_list[data_index]
         else:
             print('Map index out of range')
             return
-        plt.imshow(np.log10(np.abs(map.image)), cmap='jet')
+        plt.imshow(map, cmap='jet')
         plt.show()
     
     def plot_combined(self, data_index):
-        import matplotlib.pyplot as plt
-        data = self.data_list[data_index]
-        plt.imshow(np.log10(np.abs(data.combined.image)), cmap='jet')
+        plt.imshow(self.combined_list[data_index], cmap='jet')
         plt.show()
     
     def plot_radial_profile(self, data_index, map_indexes):
-        import matplotlib.pyplot as plt
-        data = self.data_list[data_index]
         maps = []
         if 1 in map_indexes:
-            maps.append(data.map1)
+            maps.append(self.map1_list[data_index])
         if 2 in map_indexes:
-            maps.append(data.map2)
+            maps.append(self.map2_list[data_index])
         if 3 in map_indexes:
-            maps.append(data.map3)
+            maps.append(self.map3_list[data_index])
         else:
             print('No map index selected')
             return
         for map in maps:
-            image = np.abs(map.image)
-            azimut = data.azimut
+            image = map
+            azimut = self.azimut_list[data_index]
             center = int(image.shape[0] / 2)
             image = np.rot90(image, k=azimut)[center, :]
-            plt.plot(np.log10(np.abs(image)))
+            plt.plot(image)
         plt.show()
